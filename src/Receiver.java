f9872ac9682acb502c6b0e287ce84b15db07b0f1
saus-75 <georgechieng@gmail.com>
2016-09-15 17:10:23 +1000

    receiver looks good

--- src/Receiver.java
+++ src/Receiver.java
@@ -12,21 +12,54 @@ public class Receiver {
 		
 		DatagramSocket receiver = new DatagramSocket(port);
 		receiver.setSoTimeout(500);
-		while (true) {
+		while (receiver.isConnected()) {
 			DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
 			//receive
 			receiver.receive(request);
+			byte[] byteData = request.getData();
+			String[] stringData = ByteAToStringA(byteData);
+			int senderSeqNo = Integer.parseInt(stringData[3]);
 			
 			//reply
-			Packet ack = new Packet(1, 2, true, false, null);
+			String[] ACK = heading(false, true, 1, senderSeqNo+1);
 			InetAddress clientHost = request.getAddress();
 			int clientPort = request.getPort();
-			byte[] replyBuf = null;
+			byte[] replyBuf = StringAToByteA(ACK);
 			DatagramPacket reply = new DatagramPacket(replyBuf, replyBuf.length, clientHost, clientPort);
 			receiver.send(reply);
 			
 			System.out.println("Reply Sent!");
 		}
+		receiver.close();
 	}
+	
+	public static String[] heading (boolean SYN, boolean ACK, int SeqNo, int AckNo){
+		String[] head = new String[4];
 		
+		head[0] = Boolean.toString(SYN);
+		head[1] = Boolean.toString(ACK);
+		head[2] = Integer.toString(SeqNo);
+		head[3] = Integer.toString(AckNo);
+		
+		return head;
+	}
+	
+	// Converters //
+	public static byte[] StringAToByteA (String[] source) throws IOException{
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(baos);
+		oos.writeObject(source);
+		oos.flush();
+		oos.close();
+		byte[] converted = baos.toByteArray();
+		return converted;
+	}
+	
+	public static String[] ByteAToStringA (byte[] source) throws IOException, ClassNotFoundException{
+		ByteArrayInputStream bais = new ByteArrayInputStream(source);
+		ObjectInputStream ois = new ObjectInputStream(bais);
+		String[] converted = (String[]) ois.readObject();
+		ois.close();
+		return converted;
+	}
 }
