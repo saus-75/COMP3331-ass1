57ce8eb69c96488395be985edadc31bd01fea3b1
saus-75 <georgechieng@gmail.com>
2016-09-20 17:53:38 +1000

    3 way handshake implemented

--- src/Receiver.java
+++ src/Receiver.java
@@ -2,20 +2,62 @@ import java.io.*;
 import java.net.*;
 
 public class Receiver {
-	private static final int SEQ_NUM = 31;
+	private static final int SEQ_NUM = 911;
 	public static void main(String[] args) throws Exception{
 		if (args.length != 2){
 			System.out.println("Required arguements: [Receiver_Port] [file.txt]\n");
 			return;
 		}
 		int port = Integer.parseInt(args[0]);
-		//String outputFile = args[1];
+		String outputFile = args[1];
 		
 		DatagramSocket receiver = new DatagramSocket(port);
 		
 		String whole = "";
+		int connected = 0;
+		InetAddress clientHost = null;
+		int clientPort = 0;
+		int SN = SEQ_NUM;
 		
-		while (true) {
+		//3-way handshake
+		while (connected != 1){
+			DatagramPacket SYNPacket = new DatagramPacket (new byte[1024], 1024);
+			receiver.receive(SYNPacket);
+			byte[] SYNData = SYNPacket.getData();
+			String[] SYN = ByteAToStringA(SYNData);
+			
+			System.out.println("SYN received");
+			
+			clientHost = SYNPacket.getAddress();
+			clientPort = SYNPacket.getPort();
+			
+			int senderACKno = Integer.parseInt(SYN[3]);
+			
+			if (SYN[0].equals("true")){
+				String[] SYNACK = heading(true, true, false, SN, senderACKno++);
+				byte[] SYNACKBuf = StringAToByteA(SYNACK);
+				DatagramPacket SYNACKPacket = new DatagramPacket(SYNACKBuf, SYNACKBuf.length, clientHost, clientPort);
+				receiver.send(SYNACKPacket);
+				System.out.println("SYNACK sent");
+				
+				DatagramPacket ACKPacket = new DatagramPacket (new byte[1024], 1024);
+				receiver.receive(ACKPacket);
+				byte[] ACKData = ACKPacket.getData();
+				String[] ACK = ByteAToStringA(ACKData);
+				if (ACK[1].equals("true")){
+					SN = Integer.parseInt(ACK[4]);
+					connected = 1;
+					System.out.println("Connection Established");
+				} else {
+					System.out.println("Connection Lost");
+					receiver.close();
+					return;
+				}
+			}
+			
+			
+		}
+		while (connected == 1) {
 			DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
 			//receive
 			receiver.receive(request);
@@ -24,30 +66,27 @@ public class Receiver {
 			
 			int senderSeqNo = Integer.parseInt(stringData[3]);
 			
-			//String concat
-			String back = stringData[5];
-			whole += back;
-			
-			//reply
-			String[] ACK = heading(false, true, false, SEQ_NUM, senderSeqNo+1);
-			InetAddress clientHost = request.getAddress();
-			int clientPort = request.getPort();
-			byte[] replyBuf = StringAToByteA(ACK);
-			DatagramPacket reply = new DatagramPacket(replyBuf, replyBuf.length, clientHost, clientPort);
-			receiver.send(reply);
-			
-			System.out.println("Reply Sent!");
-			if (stringData[2].equals(true)){
+			if (stringData[2].equals("false")){
+				//String concat
+				String back = stringData[5];
+				whole += back;
+				System.out.println(whole + " {" + senderSeqNo + "}");
+				
+				//reply
+				String[] ACK = heading(false, true, false, SEQ_NUM, senderSeqNo+1);
+				byte[] replyBuf = StringAToByteA(ACK);
+				DatagramPacket reply = new DatagramPacket(replyBuf, replyBuf.length, clientHost, clientPort);
+				receiver.send(reply);
+				
+				System.out.println("Reply Sent!");
+				
+			}else if (stringData[2].equals("true")){
 				receiver.close();
+				//output string
+				String trimmer = whole.trim();
+				outputText(trimmer, outputFile);
 				break;
 			}
-		}
-		
-		try{
-			String trimmer = whole.trim();
-			outputText(trimmer);
-		} catch (FileNotFoundException e){
-			System.err.println("text empty.");
 		}
 	}
 	
@@ -73,9 +112,10 @@ public class Receiver {
 	}
 	
 	//text output
-	public static void outputText(String text) throws FileNotFoundException{
-		PrintWriter out = new PrintWriter(text);
-		out.print(text);
+	public static void outputText(String text, String output) throws FileNotFoundException{
+		PrintWriter out = new PrintWriter(output);
+		System.out.println(text + "{2020202}");
+		out.println(text);
 		out.close();
 	}
 	
--- src/Sender.java
+++ src/Sender.java
@@ -3,46 +3,90 @@ import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Sender {
-	private static final int SEQ_NUM = 41;
+	private static final int SEQ_NUM = 420;
 	public static void main(String[] args) throws Exception{
 		if (args.length != 8){
 			System.out.println("Required arguements: [Receiver_Host_IP] [Receiver_Port] [file.txt] [MWS] [MMS] [Timeout] [PDrop] [seed]\n");
 			return;
 		}
-		/*args*/
-		String host = args[0];
-		InetAddress IP = InetAddress.getByName(host);
+		//args
+		InetAddress IP = InetAddress.getByName(args[0]);
 		int port = Integer.parseInt(args[1]);
 		String inputFile = args[2];
-		int MMS = Integer.parseInt(args[3]);
-		/*----*/
+		int MWS = Integer.parseInt(args[3]);
+		int MMS = Integer.parseInt(args[4]);
+		int timeout = Integer.parseInt(args[5]);
+		float PDrop = Float.parseFloat(args[6]);
+		int seed = Integer.parseInt(args[7]);
 		
-		/*get file*/
+		//get file and other initialisation
 		byte[] inputByte = Files.readAllBytes(Paths.get(inputFile));
-		/* debugging
-		String inputString = Arrays.toString(inputByte);
-		*/
-		
+		int SN = SEQ_NUM;
+		int connected = 0;
 		byte[]cutByte = new byte[MMS];
 		int bytePosition = 0;
-		/*-------*/
-		
-		/*initialise socket*/
+		int pastPos = 0;
+		Random pld = new Random(seed);
+				
+		//initialise socket
 		DatagramSocket senderSocket = new DatagramSocket();
-		/*-----------------*/
+		
+		//initialise send and receive array
+		byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[1024];
+		
+		/*3 Way handshake*/
+		if (connected != 1){
+			int ACKno = 0;
+			
+			String[]  SYNString = packet(true,false,false,SN,ACKno,null);
+			byte[] SYN = StringAToByteA(SYNString);
+			
+			//Send SYN
+			DatagramPacket SYNPacket = new DatagramPacket(SYN, SYN.length, IP, port);
+			senderSocket.send(SYNPacket);
+			System.out.println("Sent");
+			
+			//Wait for SYNACK reply
+			DatagramPacket SYNACKPacket = new DatagramPacket(new byte[1024], 1024);
+			senderSocket.setSoTimeout(timeout);
+			
+			try{
+				senderSocket.receive(SYNACKPacket);
+				byte[] SYNACKDataByte = SYNACKPacket.getData();
+				String[] SYNACK = ByteAToStringA(SYNACKDataByte);
+				if (SYNACK[0].equals("true") && SYNACK[1].equals("true")){
+					//Reply with an ACK
+					ACKno = Integer.parseInt(SYNACK[3]);
+					SN++;
+					
+					String[]  ACKString = packet(false,true,false,SN,ACKno+1,null);
+					byte[] ACK = StringAToByteA(ACKString);
+					
+					DatagramPacket ACKPacket = new DatagramPacket(ACK, ACK.length, IP, port);
+					senderSocket.send(ACKPacket);
+					
+					System.out.println("Connection Established");
+					connected = 1;
+				}
+			} catch (SocketTimeoutException e){
+				//or else close socket and exit
+				System.err.println("Connection Not Established");
+				senderSocket.close();
+				return;
+			}
+		}
+		/*---------------*/
 		
 		/*cut Strings into MMS size and send*/
-		while (bytePosition < inputByte.length){
-			
-			/*initialise send and receive array*/
-			byte[] sendData = new byte[1024];
-			byte[] receiveData = new byte[1024];
-			/*------------------------------*/
+		while (bytePosition < inputByte.length && connected == 1){
 			
 			//initialised string cut to MMS
 			String cutString;
+			
 			//position
 			int i = 0;
 			
@@ -52,23 +96,53 @@ public class Sender {
 				i++;
 				bytePosition++;
 			}
-			i = 0;
 			/*----------*/
 			
 			cutString = new String(cutByte);
 			
 			//add to packet
 			//TODO need to properly figure out seq numbering
-			String[] sendStringA = packet(false,false,false,SEQ_NUM,0,cutString);
+			String[] sendStringA = packet(false,false,false,SN,0,cutString);
+			
 			//conversion
 			sendData = StringAToByteA(sendStringA);
+			
 			//sends packet
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IP, port);
 			senderSocket.send(sendPacket);
 			System.out.println("Sent");
 			
-			//wait for ack
+			//Sanitising my int and my Byte arrays
+			i = 0;
+			int dataSize = cutByte.length;
+			cutByte = new byte[cutByte.length];
+			
+			/*wait for ack*/
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			
+			//Set timeout
+			senderSocket.setSoTimeout(timeout);
+			try {
+				senderSocket.receive(receivePacket);
+				receiveData = receivePacket.getData();
+				String[] received = ByteAToStringA(receiveData);
+				if (received[1].equals("true")){
+					SN += dataSize;
+					System.out.println("ACK received");
+					//pastPos = bytePosition;
+				}
+			} catch (SocketTimeoutException e){
+				System.err.println("Packet Lost");
+				//bytePosition = pastPos;
+			}
+			/*-------------*/
 		}
+		//Connection close
+		String[] FINA = packet(false,false,true,SN,0,null);
+		SN++;
+		byte[] FIN = StringAToByteA(FINA);
+		DatagramPacket sendPacket = new DatagramPacket(FIN, FIN.length, IP, port);
+		senderSocket.send(sendPacket);
 		senderSocket.close();
 	}
 	
